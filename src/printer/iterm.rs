5a338d7980c124e992bc11ac4108161c9b3e0a9f
Thor Correia <thor.lucas@icloud.com>
2021-03-13 17:06:35 -0800

    quick fix by simply ignoring find_best_fit

--- src/printer/iterm.rs
+++ src/printer/iterm.rs
@@ -53,14 +53,30 @@ fn print_buffer(img: &DynamicImage, img_content: &[u8], config: &Config) -> ViuR
 
     adjust_offset(&mut stdout, config)?;
 
+    // Note: find_best_fit is not necessary for iTerm2. It will already fit to the terminal size if
+    // no height or width are provided. If only one is provided, it will scale the aspect ratio
+    // appropriately. Additionally, it's calculations don't seem to be working properly with iTerm2
+    // anyway.
+    // TODO: Keeping find_best_fit here anyway just because we need a ViuResult.
+    // TODO: Maybe fix find_best_fit instead? It would be more elegant.
     let (w, h) = find_best_fit(&img, config.width, config.height);
+
+    let w_str = match config.width {
+        Some(w) => format!("width={};", w),
+        None => "".to_string(),
+    };
+
+    let h_str = match config.height {
+        Some(h) => format!("height={};", h),
+        None => "".to_string(),
+    };
 
     writeln!(
         stdout,
-        "\x1b]1337;File=inline=1;preserveAspectRatio=1;size={};width={};height={}:{}\x07",
+        "\x1b]1337;File=inline=1;preserveAspectRatio=1;size={};{}{}:{}\x07",
         img_content.len(),
-        w,
-        h,
+        w_str,
+        h_str,
         base64::encode(img_content)
     )?;
     stdout.flush()?;
